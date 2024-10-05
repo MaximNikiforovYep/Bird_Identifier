aa825bdd37d4e872816fcc18c87473b46e1301a5
MaximNikiforovYep <maxinikifor@gmail.com>
2024-10-05 19:03:37 +0300

    added onViewCreated in fragments and updated gradle

--- app/src/main/java/com/example/birdidentifier/MainFragment.java
+++ app/src/main/java/com/example/birdidentifier/MainFragment.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.birdidentifier.databinding.FragmentMainBinding;
 
